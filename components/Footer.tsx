ff94b12dbcf936b279f4b7289960eb8ebcb8dc56
rawillia9801 <163735923+rawillia9801@users.noreply.github.com>
2026-04-27 04:32:49 -0400

    Add reusable footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+ return (
+  <footer style={{padding:'50px 0',marginTop:'80px',borderTop:'1px solid rgba(255,255,255,0.1)',opacity:.7}}>
+   <p>© 2026 E-Sign Virginia — Secure Virginia Electronic Signature & Online Verification Services.</p>
+  </footer>
+ )
+}
